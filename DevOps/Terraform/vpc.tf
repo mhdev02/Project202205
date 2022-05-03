13bba08c246992d5298ae5097c85152a08d9dfcc
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-04 05:32:49 +0900

    Update vpc.tf
    #2 VPC AZ 내용 수정

--- DevOps/Terraform/vpc.tf
+++ DevOps/Terraform/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "main-public-subnet1" {
   vpc_id                  = aws_vpc.vpc-main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "ap-northeast-2"
+  availability_zone       = "ap-northeast-2a"
 
   tags = {
     Name = "main-public-1"
