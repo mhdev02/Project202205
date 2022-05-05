b0344804e8e4a60fd6a2c115ea3cc07bd20285a5
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-06 02:42:14 +0900

    Create ItemRepository.java
    #3 ItemRepository 생성

---
+++ Backend/src/main/java/com/project/backend/io/repository/ItemRepository.java
@@ -0,0 +1,15 @@
+package com.project.backend.io.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.backend.io.entity.ItemEntity;
+import com.project.backend.io.entity.UserEntity;
+
+@Repository
+public interface ItemRepository extends CrudRepository<ItemEntity, Long>{
+	List<ItemEntity> findAllByUserPurchases(UserEntity userEntity);
+	ItemEntity findByAddressId(String addressId);
+}
