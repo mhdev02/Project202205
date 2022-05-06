6395d358eb570849526e25c354d0c65eeb5b6705
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-07 05:13:20 +0900

    Update UserRepository.java
    #3 Spring Tool Suite의 Format 기능 사용

--- Backend/src/main/java/com/project/backend/io/repository/UserRepository.java
+++ Backend/src/main/java/com/project/backend/io/repository/UserRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import com.project.backend.io.entity.UserEntity;
 
 public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
-	
+
 	UserEntity findByEmail(String email);
-	UserEntity findByUserId(String userId);  // query method
+
+	UserEntity findByUserId(String userId); 
 
 }
