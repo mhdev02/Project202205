3f9d4d2052af782c799c094ac781ed14060f8e2d
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-14 01:45:55 +0900

    Create ItemRequestModel.java
    #3 아이템 생성 등을 위한 요청 모델 생성

---
+++ Backend/src/main/java/com/project/backend/ui/model/request/ItemRequestModel.java
@@ -0,0 +1,42 @@
+package com.project.backend.ui.model.request;
+
+public class ItemRequestModel {
+
+	private String itemId;
+	private String name;
+	private Integer price;
+	private Integer stock;
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+}
