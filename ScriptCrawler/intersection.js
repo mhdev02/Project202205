3647aadedfecef24159f6940f2ca8c5a805e4e98
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-15 01:16:03 +0900

    Update intersection.js
    #34 공백을 결과값에서 제거하고 단어 등장 횟수를 표시하도록 수정

--- ScriptCrawler/intersection.js
+++ ScriptCrawler/intersection.js
@@ -1,34 +1,32 @@
-exports.intersection = function(...args) {
-    let result = [];
-    let containers = [];    
+exports.intersection = function (...args) {
 
-    for(let i = 0; i < args.length; i++) {
-      let container = {};
-      for(let j = 0; j < args[i].length; j++) {
-        if(!container[args[i][j]]) container[args[i][j]] = 1;
-        else container[args[i][j]]++;
-      }
-      containers.push(container);
-    }   
+  let result = [];
+  let containers = [];
 
-    let keys = Object.keys(containers[0]);    
-
-    for(let i = 0; i <= keys.length; i++) {
-      let flag = false;
-      let value = [];
-      for(let j = 0; j < containers.length; j++) {
-        if(!containers[j][keys[i]]) {
-          flag = true;  
-        } else {
-          value.push(containers[j][keys[i]]);
-        }
-      }
-      if(!flag) {
-        for(let k = 0; k < Math.min.apply(null, value); k++) {
-          result.push([keys[i]]);
-        }
-      } 
+  for (let i = 0; i < args.length; i++) {
+    let container = {};
+    for (let j = 0; j < args[i].length; j++) {
+      if (!container[args[i][j]]) container[args[i][j]] = 1;
+      else container[args[i][j]]++;
     }
-    return result.flat();
-};
+    containers.push(container);
+  }
 
+  let keys = Object.keys(containers[0]);
+
+  for (let i = 0; i <= keys.length; i++) {
+    let flag = false;
+    let value = [];
+    for (let j = 0; j < containers.length; j++) {
+      if (!containers[j][keys[i]]) {
+        flag = true;
+      } else {
+        value.push(containers[j][keys[i]]);
+      }
+    }
+    if (!flag && (keys[i].length !== 0)) {
+      result.push([keys[i]] + ": " + Math.min.apply(null, value));
+    }
+  }
+  return result.flat();
+};
