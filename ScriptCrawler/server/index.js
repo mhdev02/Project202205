bcabf39c779b423ec6e1d823f5430e87ce8218e6
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-16 08:03:21 +0900

    Update index.js
    #36 server 폴더로 이동하고 cors, compression 라이브러리 추가

--- ScriptCrawler/server/index.js
+++ ScriptCrawler/server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const compression = require('compression');
 
 const processRouter = require('./router');
 
 const app = express();
 
+app.use(cors());
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
