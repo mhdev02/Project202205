ec23287c641bf70af0a5d5af36dfe8ed5847c78f
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-17 01:26:02 +0900

    Update crawlController.js
    #36 rate limit 기능 추가 및 공통 키워드를 로컬에서 저장하는 로직에서 응답으로 전송하도록 변경

--- ScriptCrawler/server/crawlController.js
+++ ScriptCrawler/server/crawlController.js
@@ -1,72 +1,52 @@
 const request = require("request");
 const cheerio = require("cheerio");
-const fs = require("fs");
 const Redis = require('ioredis');
 
 const redis1 = new Redis();
-const redis = redis1.duplicate();
+const redis2 = redis1.duplicate();
 
-let lists = ["\n", "\n\n", ":", "\t", "\t\t", "Copyright", "ⓒ", "All", "rights", "reserved.", "등록일자", "발행인", "편집인", "전체", "지면",
-  "로그인", "5월", "보기", "보도자료", "온라인광고", "사이트맵", "디지털초판", "개인정보취급방침"];
+let FiteringLists = ["\n", "\n\n", ":", "\t", "\t\t", "Copyright", "ⓒ", "All", "rights", "reserved.", "등록일자", "발행인", "편집인", "전체", "지면",
+  "로그인", "보기", "보도자료", "온라인광고", "사이트맵", "디지털초판", "개인정보취급방침", ","];
+
+let callCount = 0;
+
+setInterval(() => InitializeCallCount(), 300);
 
 let crawl = async function (req, res) {
-  
-  const url1 = req.body.url1;
-  const url2 = req.body.url2;
-  let contents1;
-  let contents2;
 
-  let html1 = await doRequest(url1);
-  let $ = cheerio.load(html1);
-  contents1 = $('body').text();
+  callCount++;
+  if (callCount >= 21) {
+    return res.status(429).send("You can send only 50 requests every 5 minutes");
+  };
 
-  let html2 = await doRequest(url2);
-  let $1 = cheerio.load(html2);
-  contents2 = $1('body').text();
+  try {
+    const url1 = req.body.url1;
+    const url2 = req.body.url2;
+    let contents1;
+    let contents2;
 
-  for (let i = 0; i < lists.length; i++) {
-    var regex = new RegExp(lists[i], "gi")
-    contents1 = contents1.replace(regex, ' ');
-    contents2 = contents2.replace(regex, ' ');
-  }
+    let html1 = await doRequest(url1);
+    let $ = cheerio.load(html1);
+    contents1 = $('body').text();
 
-  await redis.publish('work', JSON.stringify(contents1 + "+++" + contents2));
+    let html2 = await doRequest(url2);
+    let $1 = cheerio.load(html2);
+    contents2 = $1('body').text();
 
-  await redis.hgetall('keywords', (err, values) => {
-    let timestamp = new Date();
-    let filename = timestamp.getMonth() + '' + timestamp.getDate() + '' + timestamp.getHours() + ''
-      + timestamp.getMinutes() + '' + timestamp.getSeconds();
-    writeFile(`./data/${filename}.txt`, values.work);
-  })
-
-  res.status(201).send('ok');
-}
-
-let readFile = async function (req, res) {
-  let timestamp = req.params.timestamp;
-  let filename = `./data/${timestamp}.txt`;
-
-  if (fs.existsSync(filename)) {
-    return fs.readFile(filename, 'utf8', (err, data) => {
-      if (err) {
-        res.status(404).send('not found');
-      } else {
-        res.status(200).send({ data });
-      }
-    })
-  }
-  res.status(404).send('not found');
-}
-
-async function writeFile(filename, body) {
-  fs.writeFile(filename, body, (err) => {
-    if (err) {
-      console.log('error occured while writing')
-    } else {
-      console.log('saved!');
-
+    for (let i = 0; i < FiteringLists.length; i++) {
+      var regex = new RegExp(FiteringLists[i], "gi")
+      contents1 = contents1.replace(regex, ' ');
+      contents2 = contents2.replace(regex, ' ');
     }
-  });
+
+    await redis1.publish('work', JSON.stringify(contents1 + "+++" + contents2));
+
+    await redis2.hgetall('keywords', (err, values) => {
+      res.status(201).send(values.work);
+    })
+  } catch (e) {
+    res.status(500).send("An error occured. The server requires to check a problem.")
+  }
 }
 
 function doRequest(url) {
@@ -81,7 +61,10 @@ function doRequest(url) {
   });
 }
 
+function InitializeCallCount() {
+  callCount = 0;
+}
+
 module.exports = {
-  crawl,
-  readFile
+  crawl
 }
