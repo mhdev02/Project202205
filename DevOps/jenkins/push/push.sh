e862da959a55afdbc7c1d4c28a38277ab523b565
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 01:53:44 +0900

    Create push.sh
    #2 도커 이미지 push를 위한 스크립트 작성

---
+++ DevOps/jenkins/push/push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "***** Pushing the image to Docker Hub ***"
+
+IMAGE="backend"
+
+docker login -u $ID -p $PASSWORD
+
+docker tag $IMAGE:$BUILD_TAG $ID/$IMAGE:$BUILD_TAG
+
+docker push $ID/$IMAGE:$BUILD_TAG
