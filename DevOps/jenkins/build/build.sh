ee94e1391c74ba4b452499dfc9b0912121753681
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 07:16:48 +0900

    FIX build.sh
    #2 path 수정

--- DevOps/jenkins/build/build.sh
+++ DevOps/jenkins/build/build.sh
@@ -4,4 +4,4 @@ cp -f ./Backend/target/*.jar ./build/
 
 echo "***** Building Docker Image *****"
 
-cd .DevOps/jenkins/build/ && docker build -t backend:$BUILD_TAG . --no-cache
+cd ./DevOps/jenkins/build/ && docker build -t backend:$BUILD_TAG . --no-cache
