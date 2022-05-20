6319e6adba63c2580ebb712a3d80a28d9fd9af4c
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-21 02:43:03 +0900

    Update push.sh
    #2 server 컨테이너도 빌드하도록 설정

--- DevOps/jenkins/push/push.sh
+++ DevOps/jenkins/push/push.sh
@@ -4,12 +4,14 @@ echo "*******************************************"
 echo "***** Pushing the image to Docker Hub *****"
 echo "*******************************************"
 
-IMAGE="backend"
+IMAGE1="backend"
+IMAGE2="server"
 
 echo $PASSWORD | docker login -u $ID --password-stdin
 
-docker push $ID/$IMAGE:$BUILD_TAG
+docker push $ID/$IMAGE1:$BUILD_TAG
+docker push $ID/$IMAGE2:$BUILD_TAG
 
 echo "***** Deleting the image after pushing *****"
 
-docker rmi -f $ID/$IMAGE:$BUILD_TAG
+docker rmi -f $ID/$IMAGE1:$BUILD_TAG $ID/$IMAGE2:$BUILD_TAG
