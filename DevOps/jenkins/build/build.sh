c8f8722b0c4f0e85101a24f7ef0f26c2df811688
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 10:06:28 +0900

    Update build.sh
    #2 log 상에서 가독성을 위한 수정

--- DevOps/jenkins/build/build.sh
+++ DevOps/jenkins/build/build.sh
@@ -2,6 +2,8 @@
 
 cp -f Backend/target/*.jar DevOps/jenkins/build/
 
+echo "*********************************"
 echo "***** Building Docker Image *****"
+echo "*********************************"
 
 cd DevOps/jenkins/build/ && docker build -t $ID/backend:$BUILD_TAG . --no-cache
