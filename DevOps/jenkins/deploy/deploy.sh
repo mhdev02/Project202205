27f69d0414dd523106518aeb14dca0b41f47c944
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-21 02:44:50 +0900

    Update deploy.sh
    #2 server 컨테이너 배포를 위한 환경 변수 추가

--- DevOps/jenkins/deploy/deploy.sh
+++ DevOps/jenkins/deploy/deploy.sh
@@ -5,10 +5,13 @@ echo "***** Deploying *****"
 echo "*********************"
 
 echo backend > DevOps/jenkins/deploy/.cred
-# echo $BUILD_TAG >> DevOps/jenkins/deploy/.cred
-echo jenkins-project-15 >> DevOps/jenkins/deploy/.cred
+echo server >> DevOps/jenkins/deploy/.cred
+echo $BUILD_TAG >> DevOps/jenkins/deploy/.cred
 echo $ID >> DevOps/jenkins/deploy/.cred
 echo $PASSWORD >> DevOps/jenkins/deploy/.cred
+echo $IP >> DevOps/jenkins/deploy/.cred
+echo $REDIS_IP >> DevOps/jenkins/deploy/.cred
+echo $REDIS_PORT >> DevOps/jenkins/deploy/.cred
 
 scp -i $KEY DevOps/jenkins/deploy/.cred ec2-user@$IP:/home/ec2-user/.cred
 scp -i $KEY DevOps/jenkins/deploy/publish ec2-user@$IP:/home/ec2-user/publish
