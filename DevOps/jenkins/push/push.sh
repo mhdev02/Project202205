5b49a060cce32b4604c62f404114f2b3b3c17505
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 09:49:09 +0900

    Update push.sh
    #2 docker tag 로직 없앰

--- DevOps/jenkins/push/push.sh
+++ DevOps/jenkins/push/push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "***** Pushing the image to Docker Hub ***"
+echo "***** Pushing the image to Docker Hub *****"
 
 IMAGE="backend"
 
 docker login -u $ID -p $PASSWORD
 
-docker tag $IMAGE:$BUILD_TAG $ID/$IMAGE:$BUILD_TAG
-
 docker push $ID/$IMAGE:$BUILD_TAG
 
-sudo docker rmi -f $ID/$IMAGE:$BUILD_TAG
+echo "***** Deleting the image after pushing *****"
+
+docker rmi -f $ID/$IMAGE:$BUILD_TAG
