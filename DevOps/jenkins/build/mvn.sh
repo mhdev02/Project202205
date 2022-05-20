8aacbd319e9d1275d02d96928d5a947337eb9bbb
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 10:06:32 +0900

    Update mvn.sh
    #2 log 상에서 가독성을 위한 수정

--- DevOps/jenkins/build/mvn.sh
+++ DevOps/jenkins/build/mvn.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "************************"
 echo "***** Building jar *****"
+echo "************************"
 
 docker run --rm -v "${WORKSPACE}/Backend":/app -v "${WORKSPACE}/Backend":/root/.m2/ -w /app maven:3-alpine "$@" 
