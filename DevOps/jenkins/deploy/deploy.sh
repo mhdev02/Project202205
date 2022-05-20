baaea9f1a613816882ec42aad14963672ec32a32
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 20:37:03 +0900

    Update deploy.sh
    #2 $BUILD_TAG 원상 복귀시킴

--- DevOps/jenkins/deploy/deploy.sh
+++ DevOps/jenkins/deploy/deploy.sh
@@ -5,8 +5,7 @@ echo "***** Deploying *****"
 echo "*********************"
 
 echo backend > DevOps/jenkins/deploy/.cred
-# echo $BUILD_TAG >> DevOps/jenkins/deploy/.cred
-echo jenkins-project-15 >> DevOps/jenkins/deploy/.cred
+echo $BUILD_TAG >> DevOps/jenkins/deploy/.cred
 echo $ID >> DevOps/jenkins/deploy/.cred
 echo $PASSWORD >> DevOps/jenkins/deploy/.cred
 
