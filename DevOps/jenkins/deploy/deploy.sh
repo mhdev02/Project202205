4b1544cd11a24f3ccb794ea600c32a0f93c90f48
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 20:59:30 +0900

    Update deploy.sh
    #2 배포 자동화 테스트를 위해 임시로 주석 처리

--- DevOps/jenkins/deploy/deploy.sh
+++ DevOps/jenkins/deploy/deploy.sh
@@ -5,7 +5,8 @@ echo "***** Deploying *****"
 echo "*********************"
 
 echo backend > DevOps/jenkins/deploy/.cred
-echo $BUILD_TAG >> DevOps/jenkins/deploy/.cred
+# echo $BUILD_TAG >> DevOps/jenkins/deploy/.cred
+echo jenkins-project-15 >> DevOps/jenkins/deploy/.cred
 echo $ID >> DevOps/jenkins/deploy/.cred
 echo $PASSWORD >> DevOps/jenkins/deploy/.cred
 
