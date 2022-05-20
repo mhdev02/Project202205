18426acba778f8705f1f27a815fd9b61f4660ea5
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-21 02:52:08 +0900

    Update build.sh
    #2 path 수정

--- DevOps/jenkins/build/build.sh
+++ DevOps/jenkins/build/build.sh
@@ -7,4 +7,4 @@ echo "***** Building Docker Image *****"
 echo "*********************************"
 
 cd DevOps/jenkins/build/ && docker build -t $ID/backend:$BUILD_TAG . --no-cache
-cd ScriptCrawler/server/ && docker build -t $ID/server:$BUILD_TAG . --no-cache
+cd ../../../ScriptCrawler/server/ && docker build -t $ID/server:$BUILD_TAG . --no-cache
