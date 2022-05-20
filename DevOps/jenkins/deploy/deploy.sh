43c4820fdef36e772ab0d90e40b732cd4da33bee
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 12:53:47 +0900

    Update deploy.sh
    #2 scp key값 변경

--- DevOps/jenkins/deploy/deploy.sh
+++ DevOps/jenkins/deploy/deploy.sh
@@ -11,6 +11,6 @@ echo $BUILD_TAG >> DevOps/jenkins/deploy/.cred
 echo $ID >> DevOps/jenkins/deploy/.cred
 echo $PASSWORD >> DevOps/jenkins/deploy/.cred
 
-scp -i /opt/key DevOps/jenkins/deploy/.cred ec2-user@$IP:/home/ec2-user/.cred
-scp -i /opt/key DevOps/jenkins/deploy/publish ec2-user@$IP:/home/ec2-user/publish
-ssh -i /opt/key ec2-user@$IP "/home/ec2-user/publish" 
+scp -i $KEY DevOps/jenkins/deploy/.cred ec2-user@$IP:/home/ec2-user/.cred
+scp -i $KEY DevOps/jenkins/deploy/publish ec2-user@$IP:/home/ec2-user/publish
+ssh -i $KEY ec2-user@$IP "/home/ec2-user/publish" 
