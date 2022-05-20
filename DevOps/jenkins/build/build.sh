d5ed75086c87fdd3442e7bf2f6d727133a361214
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 09:48:34 +0900

    Update build.sh
    #2 빌드명 수정

--- DevOps/jenkins/build/build.sh
+++ DevOps/jenkins/build/build.sh
@@ -4,4 +4,4 @@ cp -f Backend/target/*.jar DevOps/jenkins/build/
 
 echo "***** Building Docker Image *****"
 
-cd DevOps/jenkins/build/ && docker build -t backend:$BUILD_TAG . --no-cache
+cd DevOps/jenkins/build/ && docker build -t $ID/backend:$BUILD_TAG . --no-cache
