9ce10f847d17675ca82b0b827c7816a8106aa561
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-20 21:08:29 +0900

    Update deploy.sh
    #2 private key를 환경 변수를 이용하여 불러오도록 변경

--- DevOps/jenkins/deploy/deploy.sh
+++ DevOps/jenkins/deploy/deploy.sh
@@ -10,6 +10,6 @@ echo jenkins-project-15 >> DevOps/jenkins/deploy/.cred
 echo $ID >> DevOps/jenkins/deploy/.cred
 echo $PASSWORD >> DevOps/jenkins/deploy/.cred
 
-scp -i /opt/key1 DevOps/jenkins/deploy/.cred ec2-user@$IP:/home/ec2-user/.cred
-scp -i /opt/key1 DevOps/jenkins/deploy/publish ec2-user@$IP:/home/ec2-user/publish
-ssh -i /opt/key1 ec2-user@$IP "/home/ec2-user/publish" 
+scp -i $KEY DevOps/jenkins/deploy/.cred ec2-user@$IP:/home/ec2-user/.cred
+scp -i $KEY DevOps/jenkins/deploy/publish ec2-user@$IP:/home/ec2-user/publish
+ssh -i $KEY ec2-user@$IP "/home/ec2-user/publish" 
