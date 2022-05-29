7482500cf729df8975408cb07b4c3603f790addd
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-05-30 04:59:30 +0900

    Update server.test.sh
    #56 가독성을 위해 echo 구문 수정

--- DevOps/jenkins/test/server.test.sh
+++ DevOps/jenkins/test/server.test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "*******************"
+echo "****************************************"
 echo "***** Testing ScriptCrawler/server *****"
-echo "*******************"
+echo "****************************************"
 
 docker run --rm $ID/server:$BUILD_TAG npm run test
