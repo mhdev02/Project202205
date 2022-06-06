eb5dbd4965ceb3cef5ad023b5e83102d4d361243
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-06 13:29:34 +0900

    Create UserControllerTest.java
    #55 UserController의 getUser()에 대한  테스트 코드 작성

---
+++ Backend/src/test/java/com/project/backend/ui/controller/UserControllerTest.java
@@ -0,0 +1,65 @@
+package com.project.backend.ui.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import com.project.backend.common.dto.ItemDto;
+import com.project.backend.common.dto.UserDto;
+import com.project.backend.service.impl.UserServiceImpl;
+import com.project.backend.ui.model.response.UserRest;
+
+public class UserControllerTest {
+	
+	@InjectMocks  
+	UserController userController;
+	
+	@Mock
+	UserServiceImpl userService;
+	
+	UserDto userDto;
+	
+	final String userId = "ZdRnAHjBmVgH2KBmhaRNAaTaDyD9t0";
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this); 		
+		
+		ItemDto itemDto = new ItemDto();
+		List<ItemDto> items = new ArrayList<>();
+		items.add(itemDto);
+		
+		userDto = new UserDto();
+        userDto.setFirstName("firstName");
+        userDto.setLastName("lastName");
+        userDto.setEmail("a@gmail.com");
+        userDto.setUserId(userId);
+        userDto.setItems(items);
+        userDto.setEncryptedPassword("AHjBmVgH2");
+		
+	}
+	
+	@Test
+	final void testGetUser() {
+	    when(userService.getUserByUserId(anyString())).thenReturn(userDto);	
+	    
+	    UserRest userRest = userController.getUser(userId);
+	    
+	    assertNotNull(userRest);
+	    assertEquals(userId, userRest.getUserId());
+	    assertEquals(userDto.getFirstName(), userRest.getFirstName());
+	    assertEquals(userDto.getLastName(), userRest.getLastName());
+	    assertTrue(userDto.getItems().size() == userRest.getItems().size());
+	}
+
+}
