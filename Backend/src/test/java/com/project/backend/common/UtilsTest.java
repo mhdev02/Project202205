646dbfec7d364d5ed1f4a1bd8e1f61f1376102ef
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-06 13:06:00 +0900

    Update UtilsTest.java
    #55 Utils.hasTokenExpired()에 대한 테스트 케이스 작성

--- Backend/src/test/java/com/project/backend/common/UtilsTest.java
+++ Backend/src/test/java/com/project/backend/common/UtilsTest.java
@@ -7,24 +7,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
-@ExtendWith(SpringExtension.class) // // loads up spring context(property files etc).  @RunWith is for JUnit 4. @ExtendWith is for JUnit 5
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UtilsTest {
-	
-	@Autowired 
+
+	@Autowired
 	Utils utils;
-	
-	@Test 
+
+	@Test
 	final void testGenerateId() {
 		String userId = utils.generateId(30);
 		String userId2 = utils.generateId(30);
-		
+
 		assertNotNull(userId);
 		assertNotNull(userId2);
-		
-		assertTrue(userId.length() == 29);
+
+		assertTrue(userId.length() == 30);
 		assertTrue(!userId.equalsIgnoreCase(userId2));
 	}
 
+	@Test
+	final void testHasTokenExpired() {
+		String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MUB0ZXN0LmNvbSIsImV4cCI6MTY1NDQ4ODU5NX0.RPdWgFlCdbCf-Ed8mPjM7g-qBhBpBBNtRTUL9B7viOwsU7fb3iFVs6bJvMKND4oNxKGg9iwIU05TnpdHadmygA";
+		boolean trueOrFalse = Utils.hasTokenExpired(token);
+
+		assertFalse(trueOrFalse);
+	}
+	
 }
