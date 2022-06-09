a9e847d3c828b02bdb60ed7323aca6b5b6f0ec7e
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-09 11:53:52 +0900

    Update mvn.sh
    #55 maven 이미지 버전 변경

--- DevOps/jenkins/test/mvn.sh
+++ DevOps/jenkins/test/mvn.sh
@@ -4,4 +4,4 @@ echo "***************************"
 echo "***** Testing Backend *****"
 echo "***************************"
 
-docker run --rm -v "${WORKSPACE}/Backend":/app -v "${WORKSPACE}/Backend":/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v "${WORKSPACE}/Backend":/app -v "${WORKSPACE}/Backend":/root/.m2/ -w /app maven:3.8.5-jdk-11 "$@"
