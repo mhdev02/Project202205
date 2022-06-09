e6995e7a58a5444ee8e0e15cf1287fde1ca0fe0a
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-09 16:45:25 +0900

    Update deploy.sh
    #70 클린코드를 위해 변수명을 더 직관적으로 변경

--- DevOps/jenkins/deploy/deploy.sh
+++ DevOps/jenkins/deploy/deploy.sh
@@ -9,7 +9,7 @@ echo server >> DevOps/jenkins/deploy/.cred
 echo $BUILD_TAG >> DevOps/jenkins/deploy/.cred
 echo $ID >> DevOps/jenkins/deploy/.cred
 echo $PASSWORD >> DevOps/jenkins/deploy/.cred
-echo $IP >> DevOps/jenkins/deploy/.cred
+echo $SERVER_IP >> DevOps/jenkins/deploy/.cred
 echo $REDIS_IP >> DevOps/jenkins/deploy/.cred
 echo $REDIS_PORT >> DevOps/jenkins/deploy/.cred
 echo $DB_URL >> DevOps/jenkins/deploy/.cred
@@ -17,8 +17,8 @@ echo $DB_USER >> DevOps/jenkins/deploy/.cred
 echo $DB_PASSWORD >> DevOps/jenkins/deploy/.cred
 echo $JWT_SECRET >> DevOps/jenkins/deploy/.cred
 
-scp -i $KEY DevOps/jenkins/deploy/.cred ec2-user@$IP:/home/ec2-user/.cred
-scp -i $KEY DevOps/jenkins/deploy/publish ec2-user@$IP:/home/ec2-user/publish
-ssh -i $KEY ec2-user@$IP "/home/ec2-user/publish" 
+scp -i $KEY DevOps/jenkins/deploy/.cred ec2-user@$SERVER_IP:/home/ec2-user/.cred
+scp -i $KEY DevOps/jenkins/deploy/publish ec2-user@$SERVER_IP:/home/ec2-user/publish
+ssh -i $KEY ec2-user@$SERVER_IP "/home/ec2-user/publish" 
 
 echo " " > DevOps/jenkins/deploy/.cred
