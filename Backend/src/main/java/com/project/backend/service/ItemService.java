08bb29da55feb062f21bd709e4128453202ef4f2
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-10 20:16:54 +0900

    Update ItemService.java
    #85 createItem() 추가

--- Backend/src/main/java/com/project/backend/service/ItemService.java
+++ Backend/src/main/java/com/project/backend/service/ItemService.java
@@ -2,9 +2,17 @@ package com.project.backend.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.RequestHeader;
+
 import com.project.backend.common.dto.ItemDto;
+import com.project.backend.common.dto.UserDto;
 
 public interface ItemService {
+	
+	boolean createItem(ItemDto item, @RequestHeader HttpHeaders headers);
 	
 	List<ItemDto> getAll();
 
