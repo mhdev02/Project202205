e243ba2d1ccc2c8f3887c000e703980cfbe76ec6
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-12 11:06:07 +0900

    Update HomeController.java
    #85 몇개의 endpoint들을 CrawlingController, ItemsController로 분리

--- Backend/src/main/java/com/project/backend/controller/HomeController.java
+++ Backend/src/main/java/com/project/backend/controller/HomeController.java
@@ -5,14 +5,16 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.project.backend.common.dto.ItemDto;
+import com.project.backend.security.SecurityConstants;
 import com.project.backend.service.ItemService;
-import com.project.backend.service.UserService;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
 
 @Controller
 public class HomeController {
@@ -20,43 +22,50 @@ public class HomeController {
 	@Autowired
 	ItemService itemService;
 	
-	@Autowired
-	UserService userService;
-	
-	@Autowired
-	private Environment env;
-	
 	@GetMapping("/")
-	public String displayItems(Model model) {
+	public String displayItemAndHome(Model model, HttpServletRequest req) {
+		
+		boolean hasJwtExpired = false;
+		String token = "";
+		
+		try {
+			String cookies = req.getHeader("Cookie");
+			
+			if (cookies != null) {
+				String[] cookiesArr = cookies.split(";");
+				for (int i = 0; i < cookiesArr.length; i++) {
+					if ("jwt".equals(cookiesArr[i].trim().split("=")[0])) {
+						token = cookiesArr[i].split("=")[1];
+					}
+				}
+			} 
+			if (token.length() > 0) {
+				token = token.replace(SecurityConstants.TOKEN_PREFIX, "");
+
+				Jwts.parser().setSigningKey(SecurityConstants.getJWTSecret()).parseClaimsJws(token).getBody()
+					.getSubject();
+			}
+
+		} catch (ExpiredJwtException e) {
+			hasJwtExpired = true;
+		}
 		
 		List<ItemDto> items = itemService.getAll();
 		model.addAttribute("itemsList", items);
+		model.addAttribute("hasJwtExpired", hasJwtExpired);
 		
-		return "home.html";
+		return "home";
 		
 	}
 	
 	@GetMapping("/register")
 	public String signUp() {
-		return "register.html";
+		return "register";
 	}
 	
 	@GetMapping("/signin")
 	public String signIn() {
-		return "signin.html";
-	}
-	
-	@GetMapping("/item")
-	public String updateItem(HttpServletRequest req) {
-		return "updateItem.html";
-	}
-	
-	@GetMapping("/crawl")
-	public String crawl(Model model) {
-	
-		model.addAttribute("SERVER_IP", env.getProperty("SERVER_IP"));
-		
-		return "crawling.html";
+		return "signin";
 	}
 	
 }
