bcdf28824822c1d66cda03232320b6dd1759b629
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-16 11:38:49 +0900

    Update script.js
    #94 중복 코드 제거를 위해 공통으로 사용되는 코드 script.js로 이동

--- Backend/src/main/resources/static/js/script.js
+++ Backend/src/main/resources/static/js/script.js
@@ -11,3 +11,59 @@ if (token.length > 0) {
 	document.getElementById('signIn').removeAttribute("style");
 	document.getElementById('signUp').removeAttribute("style");
 }
+
+function sendReq(method, itemId, redirectUrl) {
+
+	let name = $("#name").val();
+	let price = $("#price").val();
+	let quantity = $("#quantity").val();
+	let description = $("#description").val();
+	let param = { name, price, "stock": quantity, description }
+
+	if (isNaN(parseInt(price))) {
+		$('#warn').text("Price should be number");
+		return;
+	}
+	if (isNaN(parseInt(quantity))) {
+		$('#warn').text("Quantity should be number");
+		return;
+	}
+
+	$.ajax({
+		type: method,
+		data: JSON.stringify(param),
+		url: `/items/${itemId}`,
+		contentType: "application/json",
+	}).done(function(res) {
+
+		itemId = res.itemId;
+		let data = new FormData();
+		data.append("file", $("input[name=file]")[0].files[0]);
+
+		if (data.get("file") == "undefined") {
+			window.location.href = redirectUrl;
+			return;
+		}
+
+		if (method == "PUT") {
+			if (!imageId) saveImage("POST", data, itemId, "/seller");
+			else saveImage("PUT", data, imageId, "/seller");
+		} else saveImage("POST", data, itemId, "/");
+
+	})
+}
+
+function saveImage(method, data, id, redirectUrl) {
+
+	$.ajax({
+		type: method,
+		enctype: 'multipart/form-data',
+		data: data,
+		url: `/images/${id}`,
+		processData: false,
+		contentType: false,
+		cache: false,
+	}).done(function(output, status, xhr) {
+		window.location.href = redirectUrl;
+	})
+}
